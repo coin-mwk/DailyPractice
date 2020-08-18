c5d4acebc8e48a5170b908d3b0c8c0b25a5d1a06
coin-mwk <fitzdl@163.com>
2020-08-18 16:15:34 +0800

    2020-8-18

---
+++ 8.12/src/fitz/collection/LinkedListTest.java
@@ -0,0 +1,31 @@
+package fitz.collection;
+import java.util.*;
+
+/**
+ * @author Fitz
+ * @create 2020-08-18-9:07 上午
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
+        ListIterator<Object> obj = new ListIterator<>() {
+            @Override
+            public Object previous() {
+                return null;
+            }
+        };
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            if (it.next() % 2 != 0) {
+                it.remove();
+            }
+        }
+        System.out.println(list);
+    }
+}
