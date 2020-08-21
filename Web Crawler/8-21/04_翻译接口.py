68a12b1e35be3815f2c39a9fa6724588c0ddf501
coin-mwk <fitzdl@163.com>
2020-08-21 22:57:32 +0800

    2020-8-21

---
+++ Web Crawler/8-21/04_翻译接口.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : 04_翻译接口.py
+@Time    : 2020/8/21 10:56 下午
+@Author  : fitz
+
+"""
+import urllib.request
+import urllib.parse
+
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
