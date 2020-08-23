a1fd51d6d089002dd5e58713858cee6b5799d854
coin-mwk <fitzdl@163.com>
2020-08-23 22:48:57 +0800

    2020-8-23

--- 8.12/src/fitz/file/FileTest2.java
+++ 8.12/src/fitz/file/FileTest2.java
@@ -19,7 +19,17 @@ class Test2 {
     public void show03() throws IOException {
         File file2 = new File("/Users/tonystark/desktop/DailyPractice/8.12/src/1.txt");
         boolean b1 = file2.createNewFile();
-        System.out.println(1);
+        System.out.println(b1);
+        if (b1 == false) {
+//            boolean b2 = file2.delete("/Users/tonystark/desktop/DailyPractice/8.12/src/1.txt");
+        }
+    }
+
+    public void show04() {
+        File file = new File("/Users/tonystark/desktop/DailyPractice/8.12/src/aaa");
+        boolean mk = file.mkdirs();
+        System.out.println(mk);
+        System.out.println(file.delete());
     }
 }
 public class FileTest2 {
@@ -27,7 +37,8 @@ public class FileTest2 {
         Test2 test2 = new Test2();
 
 //        test2.show02();
-        test2.show03();
+//        test2.show03();
+        test2.show04();
     }
 
 }
