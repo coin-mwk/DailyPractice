0542f0bd81386f7ddaa7ac73b9d9dd81983d94f1
coin-mwk <fitzdl@163.com>
2020-08-23 19:45:17 +0800

    2020-8-23

--- 8.12/src/fitz/file/FileTest.java
+++ 8.12/src/fitz/file/FileTest.java
@@ -6,11 +6,31 @@ import java.io.File;
  * @author Fitz
  * @create 2020-08-19-12:23 下午
  */
+class Test {
+    public void show01() {
+        File file = new File("/Users/tonystark/desktop/DailyPractice");
+        System.out.println(file.getAbsoluteFile());
+        System.out.println(file.getName());
+        System.out.println(file.getPath());
+        System.out.println(file.length());
+        File file2 = new File("a.txt");
+        System.out.println(file2);
+        System.out.println(file2.getAbsoluteFile());
+    }
+}
 public class FileTest {
     public static void main(String[] args) {
-        System.out.println(File.separator);
-        System.out.println(File.pathSeparator);
-        File file = new File("/Users/tonystark/desktop/DailyPractice");
-        System.out.println(file);
+//        System.out.println(File.separator);
+//        System.out.println(File.pathSeparator);
+//      路径名真实存在时
+//        File file = new File("/Users/tonystark/desktop/DailyPractice");
+//        System.out.println(file);
+
+//        File file2 = new File("/Users/tonystark", "desktop");
+//        System.out.println(file2);
+        Test test = new Test();
+        test.show01();
+
+
     }
 }
