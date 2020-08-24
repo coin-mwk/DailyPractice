a70f8931a1cefc04dab9e9fb356f9db34f1608bc
coin-mwk <fitzdl@163.com>
2020-08-24 20:56:13 +0800

    2020-8-24

--- 8.12/src/fitz/io/FileOInputStream.java
+++ 8.12/src/fitz/io/FileOInputStream.java
@@ -1,6 +1,6 @@
 package fitz.io;
 
-import com.sun.org.apache.xpath.internal.operations.String;
+
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 import java.io.FileInputStream;
@@ -12,37 +12,36 @@ import java.util.Arrays;
  * @create 2020-08-24-11:11 上午
  */
 public class FileOInputStream {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
 //        show01();
 //        show02();
 //        show03();
     }
-
-    private static void show03() throws IOException {
-        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/c.txt");
-        byte[] bytes = new byte[1024];
-        int len = 0;//记录每次读取的有效字符个数
-        while ((len = fis.read(bytes)) != -1) {
-            System.out.println(bytes);
-        }
-    }
-
-    private static void show02() throws IOException {
-        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/c.txt");
-        byte[] bytes = new byte[5];
-        int read = fis.read(bytes);
-        System.out.println(read);
-//        System.out.println(new String(bytes));
-
-    }
-
-    private static void show01() throws IOException {
-        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/c.txt");
-        int len = 0;//记录读取的字符数
-        while ((len = fis.read())!= -1) {
-            System.out.println((char)len);
-        }
-        fis.close();
-
-    }
+//
+//    private static void show03() throws IOException {
+//        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/c.txt");
+//        byte[] bytes = new byte[1024];
+//        int len = 0;//记录每次读取的有效字符个数
+//        while ((len = fis.read(bytes)) != -1) {
+//            System.out.println(bytes);
+//        }
+//    }
+//
+//    private static void show02() throws IOException {
+//        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/c.txt");
+//        byte[] bytes = new byte[5];
+//        int read = fis.read(bytes);
+//        System.out.println(read);
+////        System.out.println(new String(bytes));
+//
+//    }
+//
+//    private static void show01() throws IOException {
+//        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/c.txt");
+//        int len = 0;//记录读取的字符数
+//        while ((len = fis.read())!= -1) {
+//            System.out.println((char)len);
+//        }
+//        fis.close();
+//    }
 }
--- 8.12/src/fitz/io/FileOutputStreamTest2.java
+++ 8.12/src/fitz/io/FileOutputStreamTest2.java
@@ -1,5 +1,7 @@
 package fitz.io;
 
+
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -9,12 +11,26 @@ import java.io.IOException;
  */
 public class FileOutputStreamTest2 {
     public static void main(String[] args) throws IOException {
-        FileOutputStreamTest2 file = new FileOutputStreamTest2();
-        file.show01();
+//        show01();
+        show02();
 
     }
 
-    private void show01() throws IOException {
+    private static void show02() throws IOException {
+        FileInputStream fis = new FileInputStream("8.12/src/fitz/io/d.txt");
+        byte[] bytes = new byte[2];
+        int len = 0; //记录每次读取的有效字节数
+        while ((len = fis.read(bytes)) != -1) {
+            System.out.println(len);
+            System.out.println(new String(bytes));
+        }
+
+        fis.close();
+
+
+    }
+
+    private static void show01() throws IOException {
         FileOutputStream fos = new FileOutputStream("8.12/src/fitz/io/d.txt", true);
         fos.write("今天学习Java了吗！\r".getBytes());
         fos.write("今天学习爬虫了吗！\r".getBytes());
