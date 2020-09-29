9e9b3546a165591b17f454982a382d854a789851
coin-mwk <fitzdl@163.com>
2020-09-29 22:18:53 +0800

    2020-9-29

--- demo/src/threadTest/MyThread.java
+++ demo/src/threadTest/MyThread.java
@@ -1,14 +1,49 @@
 package threadTest;
 
+
+import java.util.concurrent.Callable;
+
 /**
  * @author Fitz
  * @create 2020-09-29-4:42 下午
  */
-public class MyThread {
+public class MyThread implements Callable<Double>{
 
-    //计算加工成本
+    public String mesordName = "";
+    public double price = 0.0d;
 
-    //计算核算成本
+    public MyThread(String mesordName) {
+        this.mesordName = mesordName;
+    }
 
-    //计算总成本
+    public String getMesordName() {
+        return mesordName;
+    }
+
+    public void setMesordName(String mesordName) {
+        this.mesordName = mesordName;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        if (mesordName.equals("加工")){
+            //计算加工成本
+            price = 12.0d;
+            System.out.println("正在进行加工成本！");
+            System.out.println("加工成本为：" + price);
+
+        }else if (mesordName.equals("核算")) {
+            //计算核算成本
+            price = 11.0d;
+            System.out.println("正在进行核算成本！");
+            System.out.println("核算成本为：" + price);
+        }
+
+        return price;
+    }
+
+
+
+
+
 }
--- demo/src/threadTest/ThreadTest2.java
+++ demo/src/threadTest/ThreadTest2.java
@@ -1,8 +1,29 @@
 package threadTest;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author Fitz
  * @create 2020-09-29-4:44 下午
+ * 组合式异步编程
+ *
+ *
  */
 public class ThreadTest2 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+
+        //一个线程用来计算加工成本
+        FutureTask<Double> futureTask1 = new FutureTask<Double>(new MyThread("加工"));
+        new Thread(futureTask1).start();
+        System.out.println("加工成本为："+futureTask1.get());
+        //一个线程用来计算核算成本
+        FutureTask<Double> futureTask2 = new FutureTask<Double>(new MyThread("核算"));
+        new Thread(futureTask2).start();
+        System.out.println("核算成本为："+futureTask2.get());
+
+        //结果用来给总线程做最后的处理
+        System.out.println("总成本为："+(futureTask1.get() +futureTask2.get()));
+    }
 }
