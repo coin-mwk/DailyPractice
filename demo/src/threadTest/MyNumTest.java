1037f26b8d8cf28e378a850a0693eb4e427e8b63
coin-mwk <fitzdl@163.com>
2020-09-30 09:00:34 +0800

    2020-9-30

---
+++ demo/src/threadTest/MyNumTest.java
@@ -0,0 +1,18 @@
+package threadTest;
+
+/**
+ * @author Fitz
+ * @create 2020-09-30-8:59 上午
+ */
+public class MyNumTest {
+
+    public static int num = 0;
+
+    public void print0() {
+
+    }
+
+    public void print1() {
+
+    }
+}
