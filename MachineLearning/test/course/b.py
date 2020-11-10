564d66c699ca892e3890f9c8d4fd42ec9a1fface
Fitz <fitzdl@163.com>
2020-11-10 09:27:02 +0800

    2020-11-10

--- MachineLearning/test/course/b.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@File    : b.py
-@Time    : 2020/11/9 10:30 上午
-@Author  : fitz
-@Description : 
-
-"""
-a.pp()
--- MachineLearning/test/kkk/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@File    : __init__.py.py
-@Time    : 2020/11/9 2:43 下午
-@Author  : fitz
-@Description : 
-
-"""
-
