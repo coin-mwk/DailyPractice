3d5f9ff672eeb9aa6f0e1708c3a45fc725459d4b
Fitz <fitzdl@163.com>
2020-11-10 22:27:13 +0800

    2020-11-10

---
+++ MachineLearning/SVD_test/svdTest/final_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time : 2020/11/10 9:28 上午
+# @Author : Fitz
+# @Description:
+
+
+import pandas as pd
+import svd
+def main():
+    testdata = svd.loadExData()
+    print(svd.recommend(testdata, 10, N=5, percentage=0.8)) # 对编号为1的用户推荐评分较高的3件商品
+    file_path = "./../ml-1m/ratings.dat"
+    u1_data = pd.read_csv(file_path)
+    print(u1_data.head(10))
+
+if __name__ == '__main__':
+    main()
