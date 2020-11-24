4f645064bb616da40e038cd37a5f709cc75106da
Fitz <fitzdl@163.com>
2020-11-24 10:59:11 +0800

    2020-11-24

--- MachineLearning/04_SVD_vs_CF/similarityMethod.py
+++ MachineLearning/04_SVD_vs_CF/similarityMethod.py
@@ -5,31 +5,43 @@
 # @Description: 此文件描述几种推荐所用的相似度方法
 
 
-from numpy import linalg as la
-
-
-def cosineMethod(a, b):
-    from sklearn.metrics.pairwise import cosine_similarity
-    s = cosine_similarity(a, b)
-    return 1
-
-
-# 欧式距离
-def ecludSim(a, b):
-    # 这里的1/(1+距离)表示将相似度的范围放在0与1之间
-    return 1.0 / (1.0 + la.norm(a - b))
-
-
-# 皮尔逊相关系数
-def pearsSim(a, b):
-    # 皮尔逊相关系数的计算方法corrcoef()，参数rowvar=0表示对列求相似度，这里的0.5+0.5*corrcoef()是为了将范围归一化放到0和1之间=
-    if len(a) < 3:
-        return 1.0
-    return 0.5 + 0.5 * corrcoef(a, b, rowvar=0)[0][1]
+import numpy as np
 
 
 # 余弦相似度
-def cosSim(a, b):
-    num = float(a.T * b)
-    denom = la.norm(a) * la.norm(b)
-    return 0.5 + 0.5 * (num / denom)  # 将相似度归一到0与1之间
+def cos_sim(a, b):
+    a_norm = np.linalg.norm(a)
+    b_norm = np.linalg.norm(b)
+    cos = np.dot(a, b) / (a_norm * b_norm)
+    # 归一化
+    return 0.5 + 0.5 * cos
+
+
+# 欧式距离（Euclidean Distance)
+def ecl_sim(a, b):
+    # 这里的1/(1+距离)表示将相似度的范围放在0与1之间
+    ecl = np.linalg.norm(a -b)
+    # 归一化
+    return 1.0 / (1.0 + np.linalg.norm(a - b))
+
+
+# 皮尔逊相关系数 (Pearson Correlation Coefficient)
+def pea_sim(a, b):
+    pea = np.corrcoef(a, b, rowvar=0)[0][1]
+    # 归一化
+    return 0.5 + 0.5 * pea
+
+
+
+
+
+
+def main():
+    t1 = np.array([-0.4, 0.8, 0.5, -0.2, 0.3])
+    t2 = np.array([-0.5, 0.4, -0.2, 0.7, -0.1])
+    print(cos_sim(t1, t2))
+    print(ecl_sim(t1, t2))
+    print(pea_sim(t1, t2))
+
+if __name__ == '__main__':
+    main()
