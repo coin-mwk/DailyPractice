fd8978fa4762e8e89596c39d4926de3b4c5eb772
Fitz <fitzdl@163.com>
2020-12-01 21:28:23 +0800

    2020-12-1

--- MachineLearning/Scikit-Surprise/sck_01/sckTest_01.py
+++ MachineLearning/Scikit-Surprise/sck_01/sckTest_01.py
@@ -11,7 +11,6 @@ from surprise.model_selection import cross_validate
 
 # Load the movielens-100k dataset (download it if needed).
 data = Dataset.load_builtin('ml-100k')
-print(data)
 
 # Use the famous SVD algorithm.
 algo = SVD()
