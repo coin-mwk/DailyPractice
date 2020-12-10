f0817f798dcec0d78af6a172ac0814f7bfe2c382
Fitz <fitzdl@163.com>
2020-12-10 09:20:46 +0800

    2020-12-10

---
+++ MachineLearning/SVD_vs_CF/SVD/demo4.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time : 2020/12/10 9:14 上午
+# @Author : Fitz
+# @Description:
+
+
+import numpy as np
+import pandas as pd
+
+
+def demo1():
+    data = pd.read_csv('../ml-100k/u1.base', sep='\t', encoding='latin-1e', usecols=[''])
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
