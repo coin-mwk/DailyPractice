b02401d848afc1378f8bad59e8a8f6b5289642e0
Fitz <fitzdl@163.com>
2020-12-24 15:09:06 +0800

    2020-12-24

---
+++ MachineLearning/04_tensorflow/12_23/test1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time : 2020/12/23 7:48 下午
+# @Author : Fitz
+# @Description:
+import tensorflow as tf
+
+def demo1():
+
+
+
+if __name__ == '__main__':
+    demo1()
